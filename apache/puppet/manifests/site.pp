17694ff29e45351deab385482330f6962308455d
Israel Calvete <icalvete@gmail.com>
2014-04-24 10:09:19 +0200

    Special WAD virtual hosts.

--- apache/puppet/manifests/site.pp
+++ apache/puppet/manifests/site.pp
@@ -18,6 +18,16 @@ node default {
     file_uploads_size => '10M'
   }
 
+  apache2::site{'core.vhost.conf':
+    source  => 'axn-server-finder/core.vhost.conf.erb',
+    require => Class['roles::apache2_server']
+  }
+
+  apache2::site{'api.vhost.conf':
+    source  => 'axn-server/api.vhost.conf.erb',
+    require => Class['roles::apache2_server']
+  }
+
   class{'roles::syslog_sender_server':
     syslog_remote_server => '192.168.33.5'
   }
