e5b66b014a659e40ccc33da5c33e39f1afea4907
Israel Calvete <icalvete@gmail.com>
2014-07-12 10:13:18 +0200

    Update site.pp

--- mongodb/puppet/manifests/site.pp
+++ mongodb/puppet/manifests/site.pp
@@ -6,6 +6,16 @@ Stage[pre] -> Stage[main] -> Stage[post]
 node default {
 
   $environment = hiera('environment')
+  
+  $backup_dir = hiera('backup_dir')
+
+  file {'backup_dir':
+    ensure => directory,
+    path   => $backup_dir,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
 
   class {'roles::mongodb_server':}
 }
