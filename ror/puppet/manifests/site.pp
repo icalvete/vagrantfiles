50744f282d345cd2358489c1aeadf4dbfa57f281
Israel Calvete <icalvete@gmail.com>
2015-11-04 08:34:05 +0100

    Adding heroku tools to RoR development environment.

--- ror/puppet/manifests/site.pp
+++ ror/puppet/manifests/site.pp
@@ -4,4 +4,26 @@ stage{'post':}
 Stage[pre] -> Stage[main] -> Stage[post]
 
 node default {
+
+  include ntp
+
+  common::set_localtime{'set_localtime':
+    zone => 'Europe/Madrid'
+  }
+
+  common::add_env{'RAILS_ENV':
+    key   => 'RAILS_ENV',
+    value => 'development',
+  }
+  
+  common::add_env{'RACK_ENV':
+    key   => 'RAILS_ENV',
+    value => 'development',
+  }
+
+  exec { 'installing_heroku_tools':
+    command => 'wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh',
+    user    => 'root',
+    unless  => '/usr/bin/dpkg -l heroku-toolbelt | grep ii'
+  }
 }
